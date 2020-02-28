56c06e6b62be1093c17f0f4f29e9abdf57b3ca64
Sara Khandaker <kttsara@hotmail.com>
2020-02-28 20:05:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,16 @@ array
 end
 
 def reduce(array, starting_value=0)
+    i=0
+  while i<array.length
+ array[i]= yield array[i]
+  i+=1
+end
+array
+  
 end
 
 #map([1, 2, 3, -9]){|n| n * n}
+
+
+#return 
